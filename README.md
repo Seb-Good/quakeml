9eb728e7e4d129124fcd9a26c309e2e66b66b185
rosie <sebi.goodfellow@gmail.com>
2019-01-12 11:55:51 -0500

    - initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# QuakeML
+Can you predict a laboratory earthquake?
+
+## License
+[MIT](LICENSE.txt)
---
+++ setup.py
@@ -0,0 +1,14 @@
+"""Setup for QuakeML"""
+from setuptools import setup
+
+setup(
+    name='quakeml',
+    version='0.0.1',
+    description='A package for predicting laboratory earthquakes.',
+    url='https://github.com/Seb-Good/quakeml.git',
+    author='Sebastian D. Goodfellow, Ph.D.',
+    license='MIT',
+    keywords='deep learning',
+    package_dir={'': 'quakeml'},
+    zip_safe=False,
+)
